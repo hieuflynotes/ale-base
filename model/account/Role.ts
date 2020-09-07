0ff1eae1912b71978432a2ae7e2ae7936ef2b9fb
hieuflynotes <hieu@flynotes.co.uk>
2020-09-07 03:10:29 +0100

    change model for role

--- model/account/Role.ts
+++ model/account/Role.ts
@@ -1,6 +1,8 @@
 import { BaseOrganizationModel } from "../BaseOrganizationModel";
+import { Permission } from "./Permission";
 
 export class Role extends BaseOrganizationModel {
   public name: string;
   public permissionIds: string[];
+  public permissions: Permission[];
 }
